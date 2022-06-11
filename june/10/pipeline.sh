c27ec8bf67b0060b70da244a0c6cd8be355742be
Aiden Grossman <agrossman154@yahoo.com>
2022-06-11 00:06:46 -0700

    Added preliminary pipeline script and updated benchmark utilities documentation

---
+++ june/10/pipeline.sh
@@ -0,0 +1,131 @@
+# Add CMake repos so that we can use some newer
+# functions if necessary
+wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | tee /usr/share/keyrings/kitware-archive-keyring.gpg >/dev/null
+echo 'deb [signed-by=/usr/share/keyrings/kitware-archive-keyring.gpg] https://apt.kitware.com/ubuntu/ focal main' | sudo tee /etc/apt/sources.list.d/kitware.list >/dev/null
+apt-get update
+
+# install all necessary dependencies for building LLVM
+apt-get install -y \
+    python3-distutils \
+    python-is-python3 \
+    python3 \
+    python3-pip \
+    tmux \
+    g++ \
+    cmake \
+    ccache \
+    binutils-gold \
+    binutils-dev \
+    ninja-build \
+    pkg-config \
+    gcc-multilib \
+    g++-multilib \
+    gawk \
+    dos2unix \
+    libxml2-dev \
+    rsync \
+    git \
+    libtool \
+    m4 \
+    automake \
+    libgcrypt-dev \
+    liblzma-dev \
+    libssl-dev \
+    libgss-dev \
+    python3-dev \
+    wget \
+    zlib1g-dev \
+    tcl-dev
+
+# Install necessary python dependencies for building LLVM
+wget --quiet https://raw.githubusercontent.com/google/ml-compiler-opt/main/requirements.txt -P /tmp
+python3 -m pip install -r /tmp/requirements.txt
+
+# Grab tensorflow libc for compiling LLVM
+wget --quiet https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-cpu-linux-x86_64-1.15.0.tar.gz
+mkdir /tmp/tensorflow
+tar xfz libtensorflow-cpu-linux-x86_64-1.15.0.tar.gz -C /tmp/tensorflow
+
+# Clone projects
+git clone https://github.com/llvm/llvm-project.git
+git clone https://github.com/eopXD/LLVM_PGO_CMake
+
+# Unpack the given model
+# TODO(boomanaiden154): test and make sure this works
+mkdir /tmp/model_under_test
+tar xfz $1 -C /tmp/model_under_test
+
+# Build LLVM Stage1 (instrumentation)
+mkdir /llvm-stage1
+cd /llvm-stage1
+
+cmake -G Ninja \
+    -DPGO_STAGE_INSTRUMENT_INSTALL_PATH=/llvm-stage1-install \
+    -DCMAKE_C_COMPILER=gcc \
+    -DCMAKE_CXX_COMPILER=g++ \
+    -DLLVM_ENABLE_ASSERTIONS=OFF \
+    -DTENSORFLOW_C_LIB_PATH=/tmp/tensorflow \
+    -DTENSORFLOW_AOT_PATH=$(python3 -c "import tensorflow; import os; print(os.path.dirname(tensorflow.__file__))") \
+    -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=ON \
+    -DLLVM_RAEVICT_MODEL_PATH=/tmp/model_under_test \
+    -C /LLVM_PGO_Cmake/stage_instrument.cmake \
+    /llvm-project/llvm
+cmake --build .
+
+# Build LLVM Stage2 (profiling)
+mkdir /llvm-stage2
+cd /llvm-stage2
+
+cmake -G Ninja \
+    -DPGO_STAGE_INSTRUMENT_INSTALL_PATH=/llvm-stage1 \
+    -DLLVM_BINUTILS_INCDIR=/usr/include \
+    -C /LLVM_PGO_CMake/stage_profiling.cmake \
+    -DTENSORFLOW_C_LIB_PATH=/tmp/tensorflow \
+    -DTENSORFLOW_AOT_PATH=$(python3 -c "import tensorflow; import os; print(os.path.dirname(tensorflow.__file__))") \
+    -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=ON \
+    -DCMAKE_C_FLAGS="-mllvm -regalloc-enable-advisor=release -mllvm -regalloc=greedy -O3" \
+    -DCMAKE_CXX_FLAGS="-mllvm -regalloc-enable-advisor=release -mllvm -regalloc=greedy -O3" \
+    -DLLVM_RAEVICT_MODEL_PATH=/tmp/model_under_test \
+    /llvm-project/llvm
+cmake --build .
+cmake --build . --target check-llvm
+cmake --build . --target check-clang
+/llvm-stage1/bin/llvm-profdata merge -output=/profdata.prof \
+    /llvm-stage2/profiles/*.profraw
+
+# Build LLVM Stage3 (Final PGO build)
+mkdir /llvm-stage3
+cd /llvm-stage3
+
+cmake -G Ninja \
+    -DPGO_STAGE_INSTRUMENT_INSTALL_PATH=/profdata.prof \
+    -DCMAKE_C_COMPILER=/llvm-stage1/bin/clang \
+    -DCMAKE_CXX_COMPILER=/llvm-stage1/bin/clang++ \
+    -DTENSORFLOW_C_LIB_PATH=/tmp/tensorflow \
+    -DTENSORFLOW_AOT_PATH=$(python3 -c "import tensorflow; import os; print(os.path.dirname(tensorflow.__file__))") \
+    -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=ON \
+    -DLLVM_RAEVICT_MODEL_PATH=/tmp/model_under_test \
+    -C /LLVM_PGO_CMake/stage_pgo.cmake \
+    /llvm-project/llvm
+cmake --build .
+
+# Build and Run LLVM test suite
+cd /
+git clone https://github.com/llvm/llvm-test-suite.git
+
+mkdir /llvm-test-suite/build
+cd /llvm-test-suite/build
+
+cmake -G Ninja \
+    -DCMAKE_C_COMPILER="/llvm-project/build/bin/clang" \
+    -DCMAKE_CXX_COMPILER="/llvm-project/build/bin/clang++" \
+    -DTEST_SUITE_BENCHMARKING_ONLY=ON \
+    -DCMAKE_C_FLAGS="-mllvm -regalloc-enable-advisor=release -mllvm -regalloc=greedy -O3" \
+    -DCMAKE_CXX_FLAGS="-mllvm -regalloc-enable-advisor=release -mllvm -regalloc=greedy -O3" \
+    -DCMAKE_BUILD_TYPE="Release" \
+    ../
+
+/llvm-stage3/build/bin/llvm-lit -v -j $(nproc) -o results.json .
+
+# There is now a nice results.json file waiting
+# to be processed downstream.
--- june/9/llvm-benchmark-utilities.md
+++ june/9/llvm-benchmark-utilities.md
@@ -9,7 +9,7 @@ like rerun benchmarks until a statistically significant value can be
 reported, which will help eliminate a lot of the noise that will otherwise
 create a lot of confounding results. To do this:
 ```bash
-python3 process_results.py /path/to/results.json /path/to/output.csv
+python3 process_execution_time_results.py /path/to/results.json /path/to/output.csv
 ```
 This will create an `output.csv` file in the format:
 ```
@@ -20,5 +20,10 @@ run using the following script:
 ```bash
 python3 compare_results.py /path/to/output1.csv /path/to/output2.csv
 ```
-This will print info on the overall relative speedup (presented as a 
-percentage) that run2 had compared to run1.
+This will print info on the overall relative runtime (presented as a 
+percentage) that run2 had compared to run1. The same process can be
+done for analyzing compile times by substituting
+`process_execution_time_results.py` in the first command (the one for
+generating the CSVs) to `process_compile_time_results.py`. This will
+generate CSVs of the same format, but with the second column being
+compile times instead of execution times.
