6f0a2d6759000cd020a786d5ea8b6be3a3a1bc54
Aiden Grossman <agrossman154@yahoo.com>
2022-06-10 22:10:37 -0700

    Refactored execution time processor and added compile time processor

---
+++ june/9/process_compile_time_results.py
@@ -0,0 +1,24 @@
+import json
+import sys
+
+def getCompileTimes(resultsFileName):
+    output = []
+    with open(resultsFileName) as resultsFile:
+        results = json.load(resultsFile)
+        for test in results["tests"]:
+            if "compile_time" in test["metrics"]:
+                output.append((test["name"],test["metrics"]["compile_time"]))
+    return output
+
+def writeCompileTimes(results, outputFileName):
+    with open(outputFileName, "w") as outputFile:
+        for result in results:
+            testName, compileTime = result
+            outputFile.write(testName + "," + str(compileTime) + "\n")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("usage is [process_compile_time_results.py] [input file] [output file]")
+    
+    compileTimeResults = getCompileTimes(sys.argv[1])
+    writeCompileTimes(compileTimeResults, sys.argv[2])
---
+++ june/9/process_execution_time_results.py
@@ -0,0 +1,31 @@
+import json
+import sys
+
+# A very simple script that copies all executing time output data 
+# from the LLVM test suite microbenchmarks to a CSV for easier 
+# processing in the future (eg comparison to other runs with 
+# different optimizations and what not)
+
+def getExecutionTimes(resultsFileName):
+    outputs = []
+    with open(resultsFileName) as resultsFile:
+        resultsData = json.load(resultsFile)
+        for test in resultsData["tests"]:
+            if "exec_time" in test["metrics"]:
+                outputs.append((test["name"],test["metrics"]["exec_time"]))
+    return outputs
+
+def writeExecutionTimes(results, outputFileName):
+    with open(outputFileName, "w") as outputFile:
+        for output in results:
+            testName, testExecTime = output
+            outputFile.write(testName + "," + str(testExecTime) + "\n")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage is [process_results.py] [input file] [output file]")
+        sys.exit(1)
+
+    results = getExecutionTimes(sys.argv[1])
+    writeExecutionTimes(results, sys.argv[2])
+    print("Wrote data to " + sys.argv[2])
